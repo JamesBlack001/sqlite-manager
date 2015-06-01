c883967b70db1369e6f1d2effed3faee8377703c
James Black <james_black001@hotmail.co.uk>
2015-06-01 13:17:13 +0100

    update

--- sqliteManager.php
+++ sqliteManager.php
@@ -30,6 +30,8 @@ class sqliteManager
 	 */
 	private $dbh;
 	private $keyArray;
+	private $dbname;
+	private $statement;
 
 	/**
 	 * Collection of results from db
@@ -43,8 +45,9 @@ class sqliteManager
 	 */
 	function __construct($dbname)
 	{
+		$this->dbname = $dbname;
 		$this->dbh = new PDO('sqlite:'.$dbname.'sqlite3');
-		
+		$keyArray=[];
 	}
 	public function returnDBHissetVal()
 	{
@@ -62,25 +65,73 @@ class sqliteManager
 	 * for sqlite3 module, can be extended to set user required attributes.
 	 * @return Bool based on success of setting attributes
 	 */
-	public function setAttributes(){
+	public function setAttributes()
+	{
 
 		$success = $this->dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 		return $success;
+	}
+	public function getKeyArray()
+	{
+		return $this->keyArray;
 	}
 	/**
 	 * adds a key to the array keyArray readying the array for forming the insert
 	 * statement
 	 * @param String $key is a part of the array that will form the insert statement
 	 */
-	public function addKey($key){
-		if(isset($keyArray)){
+	public function addKey($key)
+	{
+		if(isset($this->keyArray)){
+			$length = sizeof($this->keyArray);
+			$this->keyArray[$length] = $key;
 			$success = True;
+
 		}
 		else{
-			$keyArray = array($key);
-			if(isset($keyArray)) {$success = True;}
+			$this->keyArray = array($key);
+			if(isset($this->keyArray)) {$success = True;}
 			else {$success = False;}
 		}
 		return $success;
+	}
+	/**
+	 * Creates the statement from the keyArray var that is used to make a qury to the db
+	 * @return [type] [description]
+	 */
+	public function createString()
+	{
+		$length = sizeof($this->keyArray);
+		$this->statement =  "CREATE TABLE IF NOT EXISTS ".$this->dbname.
+												" (";
+		
+		for ($i = 0 ; $i<($length-1) ; $i++) {
+			$this->statement .= $this->keyArray[$i] .",";
+		}
+		$this->statement .= $this->keyArray[$length-1] .")";
+		
+		//								id INTEGER PRIMARY KEY, 
+      //              title TEXT, 
+        //            message TEXT, 
+          //          time INTEGER)");
+
+		//var_dump($this->dbname);
+		//var_dump($this->keyArray);
+	}
+	public function getStatement(){
+		return $this->statement;
+	}
+	/**
+	 * uses the dbh and the statement created in createString()
+	 * and sends an SQL query that creates a table
+	 * @return Int 0 on fail, else numRows returned from result of successful statement execution
+	 */
+	public function initiateTable(){
+		$success = $this->dbh->exec($this->statement);
+		//echo $success;
+		return $success;
+	}
+	public function dropTable($dbname){
+		$this->dbh->exec("DROP TABLE ".$dbname);
 	}
 }
--- sqliteManagerTest.php
+++ sqliteManagerTest.php
@@ -4,42 +4,97 @@ include 'sqliteManager.php';
 class StackTest extends PHPUnit_Framework_TestCase
 {
     private $db;
-    
+    private $keyArray;
 
-    /*
-    public function testPushAndPop()
-    {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
-
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
-    }
-    */
+    /**
+     * Sets up the framework for the test suite
+     */
     public function setUp()
     {
         $this->db = new sqliteManager('new');
+        $this->keyArray=[];
     }
+    /**
+     * tests dedicated function in sqliteManager, that returns True
+     * if the db has been initialised properly
+     * @returned Bool The returned value from calling returnDBHissetVal() on the dbhandler
+     */
     public function testDBHsetup(){
-        $return = $this->db->returnDBHissetVal();
-        $this->assertTrue($return);
+        $returned = $this->db->returnDBHissetVal();
+        $this->assertTrue($returned);
     }
+    /**
+     * tests dedicated function in sqliteManager, that returns True if the sqlite db has its attributes set
+     * @returned [Bool] [The returned value from calling setAttributes() on the dbhandler]
+     */
     public function testAttributesSet()
     {
         //$db = new sqliteManager('new');
         
-        $return = $this->db->setAttributes();
-        $this->assertTrue($return);
+        $returned = $this->db->setAttributes();
+        $this->assertTrue($returned);
 
     }
+    /**
+     * [testKeySet tests that the method addKey returns a True. Addkey returns True if success, else false]
+     * @returned [Bool] [The returned value from calling addKey() on the dbhandler]
+     */
     public function testKeySet()
     {
-        $return = $this->db->addKey('test key');
-        $this->assertTrue($return);
+        $returned = $this->db->addKey('test key');
+        $this->assertTrue($returned);
+    }
+    /**
+     * Tests that the keys added to the keyArray in the dbhandler is correctly set, by asserting the return of those values are the same as what was input.
+     */
+    public function testGetKeyArray()
+    {
+        //assign some values to the keyarray
+        $this->db->addKey('hello');
+        $this->db->addKey('name');
+
+        //get the key set
+        
+        $this->keyArray = $this->db->getKeyArray();
+
+
+        //test it matches the values
+        $this->assertTrue('hello' == $this->keyArray[0]);
+        $this->assertTrue('name' == $this->keyArray[1]);
+        //$this->assertEquals();
+    }
+
+    public function testStringSetup()
+    {
+        //assign some values to the keyarray
+        $this->db->addKey('hello');
+        $this->db->addKey('name');
+
+        //call the dbhandler method createString()
+        $this->db->createString();
+
+        //get the string value created in createString()
+        $stmt = $this->db->getStatement();
+
+        $this->assertTrue($stmt == 'CREATE TABLE IF NOT EXISTS new (hello,name)');
+    }
+    /**
+     * Tests that the tableCreation runs successfully
+     * @return [type] [description]
+     */
+    public function testTableSetup()
+    {
+        $this->db->addKey('message TEXT');
+        $this->db->createString();
+
+        $returned = $this->db->initiateTable();
+        $this->assertTrue(0 === $returned);
+    }
+
+
+    public function tearDown()
+    {
+        //$this->db->dropTable('new');
     }
 }
 ?>
